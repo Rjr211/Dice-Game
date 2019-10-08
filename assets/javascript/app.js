9fe98ceb91ad64e5b166233f44ca1d4275b5926d
Rob <rjr211@scarletmail.rutgers.edu>
2019-10-08 14:37:20 -0400

    DOM manip for button roll

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,13 +1,37 @@
 //Variables to keep track of score
 
-var scores, roundScore, activePlayer, dice;
+var scores, roundScore, activePlayer;
 
 scores = [0,0];
 roundScore = 0;
 activePlayer = 0;
 
-dice = Math.floor(Math.random() * 6) + 1;
+//Hide dice on game load
+document.querySelector('.dice').style.display = 'none';
 
-document.querySelector('#current-' + activePlayer).textContent = dice;
+//set defaults to 0
+document.getElementById('score-0').textContent = '0';
+document.getElementById('score-1').textContent = '0';
+document.getElementById('current-0').textContent = '0';
+document.getElementById('current-1').textContent = '0';
 
-document.querySelector('.dive').style.display = 'none';
+document.querySelector('.btn-roll').addEventListener('click', function(){
+
+
+    var dice = Math.floor(Math.random() * 6) + 1;
+
+    //display the result
+    var diceDOM =  document.querySelector('.dice');
+    diceDOM.style.display = 'block';
+    diceDOM.src = 'assets/images/' + 'dice-' + dice + '.png'
+
+    //update round score IF the rolled number was not a 1
+});
+
+
+
+
+
+
+//Toggle between player and dice
+//document.querySelector('#current-' + activePlayer).textContent = dice;
