bbe433997a46366420622b177055a867aaf03084
Rob <robert.j1450@gmail.com>
2019-10-09 19:57:00 -0400

    fixed active attr. bug

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,18 +1,9 @@
 //Variables to keep track of score
 var scores, roundScore, activePlayer;
 
-scores = [0,0];
-roundScore = 0;
-activePlayer = 0;
+init();
 
-//Hide dice on game load
-document.querySelector('.dice').style.display = 'none';
 
-//set defaults to 0
-document.getElementById('score-0').textContent = '0';
-document.getElementById('score-1').textContent = '0';
-document.getElementById('current-0').textContent = '0';
-document.getElementById('current-1').textContent = '0';
 
 document.querySelector('.btn-roll').addEventListener('click', function(){
     //random number
@@ -25,12 +16,14 @@ document.querySelector('.btn-roll').addEventListener('click', function(){
 
     //update round score IF the rolled number was not a 1
     if (dice !== 1) {
+        //Add score
         roundScore += dice;
         document.querySelector('#current-' + activePlayer).textContent = roundScore;
     } else {
-       nextPlayer();
+        //Next player
+        nextPlayer();
     }
-    
+
 });
 
 document.querySelector('.btn-hold').addEventListener('click', function() {
@@ -41,11 +34,11 @@ document.querySelector('.btn-hold').addEventListener('click', function() {
     document.querySelector('#score-' + activePlayer).textContent = scores[activePlayer];
 
     //check if player won the game
-    if (scores[activePlayer] >= 20) {
-        document.querySelector('#name-' + activePlayer).innerHTML = "<b>" + 'Winner!!' + "</b>"
+    if (scores[activePlayer] >= 10) {
+        document.querySelector('#name-' + activePlayer).textContent = "Winner!!"
         document.querySelector('.dice').style.display = 'none';
-        document.querySelector('.player-' +activePlayer + '-panel').classLista.add('winner');
-        document.querySelector('.player-' +activePlayer + '-panel').classLista.remove('active');
+        document.querySelector('.player-' + activePlayer + '-panel').classList.add('winner');
+        document.querySelector('.player-' + activePlayer + '-panel').classList.remove('active');
     } else {
         //next 
         nextPlayer();
@@ -56,17 +49,42 @@ document.querySelector('.btn-hold').addEventListener('click', function() {
 
 
 function nextPlayer() {
-    activePlayer === 0 ? activePlayer = 1 : activePlayer = 0;
+        activePlayer === 0 ? activePlayer = 1 : activePlayer = 0;
         roundScore = 0;
 
         document.getElementById('current-0').textContent = '0';
         document.getElementById('current-1').textContent = '0';
 
         document.querySelector('.player-0-panel').classList.toggle('active');
-        document.querySelector('.player-1-panel').classList.toggle('active');
+        document.querySelector('.player-1-panel').classList.toggle('active'); 
 
         document.querySelector('.dice').style.display = 'none';
-};
+}
+
+//reset game
+document.querySelector('.btn-new').addEventListener('click', init);
+
+function init() {
+    scores = [0,0];
+    activePlayer = 0;
+    roundScore = 0;
+
+    //Hide dice on game load
+    document.querySelector('.dice').style.display = 'none';
+
+    //set defaults to 0
+    document.getElementById('score-0').textContent = '0';
+    document.getElementById('score-1').textContent = '0';
+    document.getElementById('current-0').textContent = '0';
+    document.getElementById('current-1').textContent = '0';
+    document.getElementById('name-0').textContent = 'Player 1';
+    document.getElementById('name-1').textContent = 'Player 2';
+    document.querySelector('.player-0-panel').classList.remove('winner');
+    document.querySelector('.player-1-panel').classList.remove('winner');
+    document.querySelector('.player-0-panel').classList.remove('active');
+    document.querySelector('.player-1-panel').classList.remove('active');
+    document.querySelector('.player-0-panel').classList.add('active');
+} 
 
 //Toggle between player and dice
 //document.querySelector('#current-' + activePlayer).textContent = dice
